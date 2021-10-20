6e38967444eab6c49504fbf936a0004aecf08f93
parviz00 <parvizmemmedli13@gmail.com>
2021-10-20 13:02:48 +0400

    ders sonu 1

---
+++ src/app.js
@@ -0,0 +1,9 @@
+console.log("Merhaba kodlama.io")
+
+let konutKredileri=["Konut Kredisi","Emlak konut kredileri","Kamu Konut Kredisi","Ozel Konut Kredisi"]
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+console.log("</ul>")
